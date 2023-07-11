7ddc93f7aaa76a327d206539f47f40643cd968a3
ShakPind <kelvinpindahman@gmail.com>
2023-07-11 08:20:07 +0800

    new file:   .gitignore new file:   README.md new file:   package-lock.json new file:   package.json new file:   public/favicon.ico new file:   public/index.html new file:   public/logo192.png new file:   public/logo512.png new file:   public/manifest.json new file:   public/robots.txt new file:   src/Testing.js new file:   src/index.js

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function Greeting() {
+    return (
+        <div>
+            <h2>john doe</h2>
+            <p>this is my message</p>
+    </div>
+    );
+}
+const Person = () => <h2>john doe</h2>;
+const Message = () => {
+    return <p>this is my message</p>;
+};
+
+//const Greeting = () => {
+//  return React.createElement('h2', {}, 'hello world');
+//};
+
+
+//function Greeting() {
+//  return (
+//    <div>
+//      <h2>hello world</h2>
+//    </div>
+//  );
+//}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<Greeting />);
